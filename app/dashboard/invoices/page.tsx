42a766715ac4e778b1dfa5e48497c33633fc27b8
Maxime Pruvost <krokhackjv@gmail.com>
2024-12-03 11:50:08 +0100

    Initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>Greetings fellow tax frauder</p>;
+}
---
+++ app/ui/fonts.tsx
@@ -0,0 +1,10 @@
+import { Roboto, Bungee } from 'next/font/google';
+
+export const roboto = Roboto({
+    weight: ['400'],
+    subsets: ['latin'],
+});
+
+export const bungee = Bungee({
+    weight: ['400'],
+})
